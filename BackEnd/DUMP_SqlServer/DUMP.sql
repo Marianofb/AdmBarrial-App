f31e3dbe882a63d8a35740e285857e054a08008a
Marianofb <maonafb@gmail.com>
2024-07-16 11:24:10 -0300

    plus

--- BackEnd/DUMP_SqlServer/DUMP.sql
+++ BackEnd/DUMP_SqlServer/DUMP.sql
@@ -1,5 +1,5 @@
 RESTORE DATABASE [Nuevo_AdmBarrial]
-FROM DISK = 'C:\Users\TuUsuario\DAI1_TPO_CODIGO\DUMP_SqlServer\AdmBarrial.bak'
+FROM DISK = 'C:\Users\maona\OneDrive\Desktop\DAI1_TPO_CODIGO\BackEnd\DUMP_SqlServer\AdmBarrial.bak'
 WITH 
     MOVE 'Municipio' TO 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\Nuevo_Municipio.mdf',
     MOVE 'Municipio_log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\Nuevo_Municipio_log.ldf',
